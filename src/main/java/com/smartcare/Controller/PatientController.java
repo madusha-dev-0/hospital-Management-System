d80687db51e402aece6d40b8b2305a3d6ad8413a
madusha-dev-0 <kumaramadushan216@gmail.com>
2026-08-19 15:32:20 +0530

    Add department

--- src/main/java/com/smartcare/Controller/PatientController.java
+++ src/main/java/com/smartcare/Controller/PatientController.java
@@ -3,7 +3,6 @@ package com.smartcare.Controller;
 import com.smartcare.Entity.PatientEntity;
 import com.smartcare.Services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
